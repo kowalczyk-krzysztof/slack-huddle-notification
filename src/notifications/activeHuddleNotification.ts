abd249335f4717b3b1d197f885fef2ca138430f9
Krzysztof Kowalczyk <kowalczykkrzysztof893@gmail.com>
2022-08-15 15:10:11 +0200

    Code cleanup (#7)

--- src/notifications/activeHuddleNotification.ts
+++ src/notifications/activeHuddleNotification.ts
@@ -1,37 +1,38 @@
 import { app } from '..';
 import { HuddleDocument, HuddleInfo } from '../lib';
 
-// TODO: Replace this and logic realted to this with real database
+// TODO: Replace this and logic related to this with real database
 const db = new Map<string, HuddleDocument>();
 export const userDb = new Map<string, boolean>();
 
-// TODO: Make this happen on first iteration
 const sendNotifications = async (huddle: HuddleDocument) => {
-  userDb.forEach(async (value, key) => {
-    // Only send to people who are not in huddle already
-    if (!huddle.members?.includes(key) && value) {
-      await app.client.chat.postMessage({
-        channel: key,
-        text: `Huddle has started`,
-      });
-    }
+  await Promise.all(
+    Array.from(userDb).map(async ([key, value]: [string, boolean]) => {
+      if (!huddle.members?.includes(key) && value) {
+        await app.client.chat.postMessage({
+          channel: key,
+          text: `Huddle has started`,
+        });
+      }
+    })
+  ).then(() => {
+    db.set(huddle.call_id, { ...huddle, notificationSent: true });
   });
-  // TODO: Promise all this
-  db.set(huddle.call_id, { ...huddle, notificationSent: true });
 };
 
 export const activeHuddleNotification = async (activeHuddle?: HuddleInfo) => {
   if (activeHuddle) {
     const { call_id, active_members } = activeHuddle;
     const huddle = db.get(call_id);
-    if (huddle) {
-      if (!huddle.notificationSent) sendNotifications(huddle);
-    } else
+    if (huddle && !huddle.notificationSent) sendNotifications(huddle);
+    else if (!huddle) {
       db.set(call_id, {
         call_id,
         members: active_members,
         notificationSent: false,
         hasEnded: false,
       });
+      sendNotifications(db.get(call_id) as HuddleDocument);
+    }
   }
 };
